972965e01a3fa5036c9e26eec6c8c129edbc510c
Damjan Kuznar <damjan.kuznar@gmail.com>
2024-01-20 21:39:28 +0100

    Fix update_gql_schema.py script

--- README.md
+++ README.md
@@ -124,30 +124,10 @@ asyncio.run(print_orders())
 
 ### Update GraphQL schema
 
-Download the schema from:
-https://studio.apollographql.com/public/Tensor-Trade-API/variant/current/schema/sdl
-
-> Currently not allowed to introspect schema so unable to automate the following.
-
 To update the schema from Tensor.trade GraphQL API run the following command:
 
 ```bash
 poetry run update_gql_schema
-```
-
-This currenlty errors with:
-
-```json
-{
-  "errors": [
-    {
-      "message": "GraphQL introspection is not allowed by Apollo Server, but the query contained __schema or __type. To enable introspection, pass introspection: true to ApolloServer in production",
-      "extensions": {
-        "code": "GRAPHQL_VALIDATION_FAILED"
-      }
-    }
-  ]
-}
 ```
 
 ### Autogenerate clients
--- scripts/update_gql_schema.py
+++ scripts/update_gql_schema.py
@@ -1,23 +1,25 @@
-import json
+import httpx
 
-import dotenv
-import os
-from graphql import get_introspection_query, build_client_schema, print_schema
 
-from tensortradesdk.sync.base_client import BaseClient
-
-dotenv.load_dotenv(".env")
-api_key = os.environ["TENSOR_API_KEY"]
+url = "https://graphql.api.apollographql.com/api/graphql"
+headers = {
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Content-Type": "application/json",
+}
+json_data = {
+    "operationName": "Studio__IntrospectionQuery",
+    "variables": {"serviceId": "Tensor-Trade-API", "schemaTag": "current"},
+    "extensions": {
+        "persistedQuery": {
+            "version": 1,
+            "sha256Hash": "15a8d6ef9a8e696f1afda1fa917b6d374036fa13893a4286634041fbf2330e8f",
+        }
+    },
+}
 
 
 def run():
-    client = BaseClient(api_key)
-    query = get_introspection_query(descriptions=True)
-    response = client.execute(query).json()
-    if errors := response.get("errors"):
-        raise RuntimeError(json.dumps(errors, indent="  "))
-
-    client_schema = build_client_schema(response.get("data", None))
-    sdl = print_schema(client_schema)
+    response = httpx.post(url, headers=headers, json=json_data)
     with open("resources/schema.graphql", "w") as schema_file:
-        schema_file.write(sdl)
+        schema_file.write(response.json()["data"]["service"]["schema"]["document"])
